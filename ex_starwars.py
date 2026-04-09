847027241c0959fb1c0c5a934dbaab46e7bc2f30
ithanara <atamadiva@gmail.com>
2026-04-09 11:20:42 -0300

    Aula 5 Ex-CloneWars

--- ex_starwars.py
+++ ex_starwars.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_parquet("data/dados_clones.parquet")
 df.head()
 # %%
-features = [ 'Massa(em kilos)',	'Estatura(cm)','General Jedi encarregado' ]
+features = [ 'Massa(em kilos)',	'Estatura(cm)' ]
 target = 'Status '
 
 X = df[features]
@@ -29,5 +29,6 @@ import matplotlib.pyplot as plt
 plt.figure(dpi=400)
 tree.plot_tree(model,feature_names=features, 
                class_names=model.classes_,
-               filled=True)
+               filled=True,
+               max_depth=3)
 # %%
