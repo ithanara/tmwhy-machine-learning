17c479d530e6fdaa1ca3eec9f034cecac4f999c6
ithanara <atamadiva@gmail.com>
2026-04-24 10:18:02 -0300

    Aula 20

--- churn/train.py
+++ churn/train.py
@@ -75,3 +75,68 @@ feature_importances['acum.'] = feature_importances[0].cumsum()
 feature_importances[feature_importances['acum.'] < 0.96]
 
 # %%
+best_features = (feature_importances[feature_importances['acum.'] < 0.96]['index']
+                 .tolist())
+best_features
+# %%
+#Modify (aqui foi uma versão simples do modify)
+from feature_engine import discretisation
+
+tree_discretisation = discretisation.DecisionTreeDiscretiser(
+    variables=best_features,
+    regression=False,
+    bin_output='bin_number',
+    cv=3
+    )
+
+tree_discretisation.fit(X_train[best_features], y_train)
+# %%
+#aplicar o que foi aprendido
+X_train_transform = tree_discretisation.transform(X_train[best_features])
+X_train_transform
+# %%
+#Model
+from sklearn import linear_model
+
+reg = linear_model.LogisticRegression(
+    penalty=None,
+    random_state=42,
+    max_iter=1000000
+)
+reg.fit(X_train_transform, y_train)
+# %%
+from sklearn import metrics
+
+y_train_predict = reg.predict(X_train_transform)
+y_train_proba = reg.predict_proba(X_train_transform)[:,1]
+
+acc_train = metrics.accuracy_score(y_train, y_train_predict)
+auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+
+print("Acuracia treino:", acc_train)
+print("AUC treino:", auc_train)
+
+#Verificar dados do test
+X_test_transform = tree_discretisation.transform(X_test[best_features])
+
+y_test_predict = reg.predict(X_test_transform)
+y_test_proba = reg.predict_proba(X_test_transform)[:,1]
+
+acc_test = metrics.accuracy_score(y_test, y_test_predict)
+auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+
+print("Acuracia teste:", acc_test)
+print("AUC teste:", auc_test)
+#Ver como fica na OOT
+
+oot_transform = tree_discretisation.transform(oot[best_features])
+
+y_oot_predict = reg.predict(oot_transform)
+y_oot_proba = reg.predict_proba(oot_transform)[:,1]
+
+acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
+auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
+
+print("Acuracia oot:", acc_oot)
+print("AUC oot:", auc_oot)
+# %%
