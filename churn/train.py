84b3d57e32e05f1b2937c10697c54300f20e51e2
ithanara <atamadiva@gmail.com>
2026-05-04 12:11:59 -0300

    Aula 24

--- churn/train.py
+++ churn/train.py
@@ -1,5 +1,16 @@
 #%%
 import pandas as pd
+from sklearn import tree
+from sklearn import model_selection
+from sklearn import ensemble
+from sklearn import pipeline
+from sklearn import metrics
+
+import mlflow
+
+from feature_engine import discretisation, encoding
+
+import matplotlib.pyplot as plt
 
 pd.options.display.max_columns = 500
 pd.options.display.max_rows = 500
@@ -9,8 +20,6 @@ df.head()
 # %%
 #OOT = out of time
 oot = df[df["dtRef"] == df["dtRef"].max()].copy()
-oot
-# %%
 df_train = df[df["dtRef"] < df["dtRef"].max()].copy()
 df_train.shape
 # %%
@@ -20,19 +29,19 @@ target = 'flagChurn'
 X, y = df_train[features], df_train[target]
 # %%
 #Sample
-from sklearn import model_selection
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y,
                                                                     random_state=42,
                                                                     test_size=0.2,
-                                                                    stratify=y
+                                                                    stratify=y,
                                                                     )
 # %%
-print("taxa variável resposta:", y_train.mean())
-print("taxa variável resposta:", y_test.mean())
+print("Taxa variável resposta geral:", y.mean())
+print("Taxa variável resposta Treino:", y_train.mean())
+print("Taxa variável resposta Test:", y_test.mean())
 # %%
 #Aqui começa o Explore (Análise exploratória dos dados)
-X_train.isna().sum()
+X_train.isna().sum().sort_values(ascending=False)
 # %%
 #Analise bivariada
 df_analise = X_train.copy()
@@ -44,12 +53,9 @@ sumario
 #Diferença absoluta
 sumario['diff_abs'] = sumario[0] - sumario[1]
 sumario['diff_rel'] = sumario[0] / sumario[1]
-sumario.sort_values(by=['diff_rel'], ascending=True)
+sumario.sort_values(by=['diff_rel'], ascending=False)
 
 # %%
-from sklearn import tree
-import matplotlib.pyplot as plt
-
 arvore = tree.DecisionTreeClassifier(random_state= 42, 
                                      max_depth=5)
 arvore.fit(X_train, y_train)
@@ -71,7 +77,6 @@ feature_importances = (pd.Series(arvore.feature_importances_,
                                 .reset_index()
                                 )
 feature_importances['acum.'] = feature_importances[0].cumsum()
-#feature_importances[feature_importances[0] > 0.01]
 feature_importances[feature_importances['acum.'] < 0.96]
 
 # %%
@@ -80,9 +85,6 @@ best_features = (feature_importances[feature_importances['acum.'] < 0.96]['index
 best_features
 # %%
 #Modify (aqui foi uma versão simples do modify)
-from feature_engine import discretisation, encoding
-from sklearn import pipeline
-
 #Discretização
 tree_discretisation = discretisation.DecisionTreeDiscretiser(
     variables=best_features,
@@ -91,20 +93,12 @@ tree_discretisation = discretisation.DecisionTreeDiscretiser(
     cv=3
     )
 
-tree_discretisation.fit(X_train[best_features], y_train)
-
 #One hot encoding
 onehot = encoding.OneHotEncoder(variables=best_features, ignore_format=True)
-
-# X_train_transform = onehot.transform(X_train_transform)
-# X_train_transform
 # %%
 #Model
-from sklearn import linear_model
-from sklearn import naive_bayes
-from sklearn import ensemble
-from sklearn import metrics
-import mlflow
+mlflow.set_tracking_uri("http://127.0.0.1:3000/")
+mlflow.set_experiment(experiment_id=1)
 
 #model = linear_model.LogisticRegression(penalty=None,random_state=42,max_iter=1000000)
 #model = naive_bayes.BernoulliNB()
@@ -129,50 +123,49 @@ params = {
     "Model__criterion":['gini','entropy','log_loss']
 }
 
-
 grid = model_selection.GridSearchCV(model_pipeline, 
                                     params, 
                                     cv=3, 
                                     scoring='roc_auc',
                                     verbose=4,)
 
-mlflow.set_tracking_uri("http://127.0.0.1:3000/")
-mlflow.set_experiment(experiment_id=1)
 
 with mlflow.start_run(run_name=model.__str__()):
     mlflow.sklearn.autolog()
-    grid.fit(X_train, y_train)
 
-    y_train_predict = grid.predict(X_train)
-    y_train_proba = grid.predict_proba(X_train)[:,1]
+    model_pipeline.fit(X_train[best_features], y_train)
+
+    y_train_predict = model_pipeline.predict(X_train[best_features])
+    y_train_proba = model_pipeline.predict_proba(X_train[best_features])[:,1]
 
     acc_train = metrics.accuracy_score(y_train, y_train_predict)
     auc_train = metrics.roc_auc_score(y_train, y_train_proba)
     roc_train = metrics.roc_curve(y_train, y_train_proba)
-    print("Acuracia treino:", acc_train)
-    print("AUC treino:", auc_train)
+    print("Acurácia Treino:", acc_train)
+    print("AUC Treino:", auc_train)
 
     #Verificar dados do test
-    y_test_predict = grid.predict(X_test)
-    y_test_proba = grid.predict_proba(X_test)[:,1]
+    y_test_predict = model_pipeline.predict(X_test[best_features])
+    y_test_proba = model_pipeline.predict_proba(X_test[best_features])[:,1]
 
     acc_test = metrics.accuracy_score(y_test, y_test_predict)
     auc_test = metrics.roc_auc_score(y_test, y_test_proba)
     roc_test = metrics.roc_curve(y_test, y_test_proba)
-    print("Acuracia teste:", acc_test)
-    print("AUC teste:", auc_test)
+    print("Acurácia Test:", acc_test)
+    print("AUC Test:", auc_test)
 
     #Ver como fica na OOT
-    y_oot_predict = grid.predict(oot[features])
-    y_oot_proba = grid.predict_proba(oot[features])[:,1]
+    y_oot_predict = model_pipeline.predict(oot[best_features])
+    y_oot_proba = model_pipeline.predict_proba(oot[best_features])[:,1]
 
     acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
     auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
     roc_oot = metrics.roc_curve(oot[target], y_oot_proba)
-    print("Acuracia oot:", acc_oot)
+    print("Acurácia oot:", acc_oot)
     print("AUC oot:", auc_oot)
 
     mlflow.log_metrics({
+        "acc_train":acc_train,
         "auc_train" : auc_train,
         "acc_test" : acc_test,
         "auc_test" : auc_test,
@@ -193,4 +186,11 @@ plt.legend([
     f"Teste: {100*auc_test: .2f}",
     f"OOT: {100*auc_oot: .2f}",
 ])
+# %%
+model_df = pd.Series({
+    "model": model_pipeline,
+    "features": best_features,
+})
+
+model_df.to_pickle("model.pkl")
 # %%
